f374ac57a84e93b389861cf0dbfdb4981a3bdce6
= <=>
2018-09-08 15:42:28 -0600

    [Unit 6 Exercise 03] Spring Word Count: Config

--- src/main/java/com/longhai/playground/config/WordConfig.java
+++ src/main/java/com/longhai/playground/config/WordConfig.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Component
 @ConfigurationProperties(prefix = "wordcount")
 public class WordConfig {
+    //Spring Config
     private boolean caseSensitive;
     private List<String> skip;
 
